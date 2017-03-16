22a74cdf013c58286814d0c172f266d04bedae71
Jakub Kudzia <jakub.kudzia@outlook.com>
2017-03-16 01:36:59 +0100

    initial setup of project

---
+++ README.md
@@ -0,0 +1,9 @@
+erlRoadTraffic
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ src/erlRoadTraffic_sup.erl
@@ -0,0 +1,37 @@
+%%%-------------------------------------------------------------------
+%%% @author Jakub Kudzia
+%%% @doc
+%%% erlRoadTraffic top level supervisor.
+%%% @end
+%%%-------------------------------------------------------------------
+-module(erlRoadTraffic_sup).
+-author("Jakub Kudzia").
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%====================================================================
+%% API functions
+%%====================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%====================================================================
+%% Supervisor callbacks
+%%====================================================================
+
+%% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
+init([]) ->
+    {ok, { {one_for_all, 0, 1}, []} }.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
